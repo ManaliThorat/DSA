1548038eba57a70bcb14922932f629910d830093
Manali <manu@RLB.com>
2013-12-16 08:10:31 +0530

    committing with isort n stack without memcpy

---
+++ isort/isortTest.c
@@ -0,0 +1,71 @@
+#include "testUtils.h"
+#include "isort.h"
+#include <stdio.h>
+#include <memory.h>
+
+typedef struct {
+        int accountNumber;
+        int balance;
+} Account;
+int areAccountsEqual(Account* expected,Account* actual){
+        int result = expected->accountNumber == actual->accountNumber && 
+                                        expected->balance == actual->balance;
+    return result;
+}
+
+int compareAccountsByAccountNumber(void* first, void* second) {
+        Account* firstAccount = (Account*)first;
+        Account* secondAccount = (Account*)second;
+        return firstAccount->accountNumber - secondAccount->accountNumber;
+}
+int compareAccountsByBalance(void* first, void* second) {
+        Account* firstAccount = (Account*)first;
+        Account* secondAccount = (Account*)second;
+        return firstAccount->balance - secondAccount->balance;
+}
+int compareInt ( void* a,  void* b){
+    return (*(int*)a - *(int*)b);
+}
+int compareDouble( void* a,  void* b){
+    return (*(double*)a - *(double*)b);
+}
+int compareFloat( void* a,  void* b){
+    return (*(float*)a - *(float*)b);
+}
+int compareChar( void* a,  void* b){
+    return (*(char*)a - *(char*)b);
+}
+int compareString(void* key,void* element){
+    return strcmp( *(String*)key, *(String*)element );
+}
+void test_1_sorting_an_array_of_integers(){
+    int expected[] = {1,2,3,4};
+    int actual[] = {4,3,1,2};
+    isort(actual, 4, sizeof(int), compareInt);
+    ASSERT(0 == memcmp(expected, actual, sizeof(expected)));
+}
+void test_2_sorting_an_array_of_double(){
+    double expected[] = {1.000000,2.000000,3.000000,4.000000};
+    double actual[] = {4.000000,3.000000,1.000000,2.000000};
+    isort(actual, 4, sizeof(double), compareDouble);
+    ASSERT(0 == memcmp(expected, actual, sizeof(expected)));
+}
+void test_3_sorting_an_array_of_float(){
+    float expected[] = {1.0,2.0,3.0,4.0};
+    float actual[] = {4.0,3.0,1.0,2.0};
+    isort(actual, 4, sizeof(float), compareFloat);
+    ASSERT(0 == memcmp(expected, actual, sizeof(expected)));
+}
+void test_4_sorting_an_array_of_character(){
+    char expected[] = {'d','b','c','a'};
+    char actual[] = {'a','b','c','d'};
+    isort(actual, 4, sizeof(char), compareChar);
+    ASSERT(0 == memcmp(expected, actual, sizeof(expected)));
+}
+
+void test_5_sorting_an_array_of_string(){
+    String expected[] = {"d","b","c","a"};
+    String actual[] = {"a","b","c","d"};
+    isort(actual, 4, sizeof(String), compareString);
+    ASSERT(0 == memcmp(expected, actual, sizeof(expected)));
+}
---
+++ stack_without_memcpy/stack.c
@@ -0,0 +1,65 @@
+#include "stack.h"
+#include <stdlib.h>
+Node* createNode(void* previous,void* next){
+        Node* node = calloc(1,sizeof(Node));
+        node->previous = previous;
+        node->next = next;
+        return node;
+}
+List* create(){
+        List* list = calloc(1,sizeof(List));
+        list->length = 0;
+        list->head = NULL;
+        return list;
+}
+int add(List* dList,int index,void* element){
+        int i=1;
+        Node* nodeToAdd = createNode(NULL,NULL);
+        Node *previousNode;
+        Node *nextNode = NULL;
+        if(index > dList->length) return 0;
+        nodeToAdd->data = element;
+        if(dList->head == NULL){
+                dList->head = nodeToAdd;
+                dList->length++;
+                return 1;
+        }
+        previousNode = dList->head;
+        while(i < index){
+                previousNode = previousNode->next;
+                if(previousNode!= NULL)
+                    nextNode = previousNode->next;
+                i++;
+        }
+        previousNode->next = nodeToAdd;
+        if(nextNode != NULL)
+            nextNode->previous = nodeToAdd;
+        nodeToAdd->previous = previousNode;
+        nodeToAdd->next = nextNode;
+        dList->length++;
+        return 1;
+}
+void* removeElement(List* list,int index){
+        int i;
+        Node *head;
+        void* data;
+        if(index > list->length-1)
+                return 0;
+        head = list->head;
+        for(i=0;i < index ;i++){
+            head = head->next;
+        }
+        data = head->data;
+        if(i==0){   
+            list->length--;
+        }
+        if(i==list->length-1){
+            head->previous = NULL;
+            list->length--;
+        }
+        head->previous = head->next;
+        head->next = head->previous;
+    free(head);
+    list->length--;
+    return data;
+};
